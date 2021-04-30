ae279ea68c897e729c5ca7489a69ca0a752ef4e5
Octavio Amietta <octavioa1997@gmail.com>
2021-04-30 12:10:48 -0300

    change: Home styles

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -2,20 +2,16 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Home.css";
 import logo from "assets/logo.png";
-import summerSale from "assets/portada.png";
 
 export default function Home() {
   return (
-    <div className="Home">
-   
+    <>
       <img src={logo} alt="" className="logo" />
-     
-      <div className="portada">
-        <img src={summerSale} alt="" className="summerSale" />
+      <div className="Home">
         <Link to="/categories">
           <button className="btn">SHOP NOW</button>
         </Link>
       </div>
-    </div>
+    </>
   );
 }
